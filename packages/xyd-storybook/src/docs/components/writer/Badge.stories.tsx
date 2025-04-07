faf10d4c4b8b851d2fc1e2ddeaeece978078b225
Patryk Zdunowski <zdunekhere@gmail.com>
2025-04-07 22:18:29 +0200

    fix storybook

--- packages/xyd-storybook/src/docs/components/writer/Badge.stories.tsx
+++ packages/xyd-storybook/src/docs/components/writer/Badge.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Badge, type BadgeStyles } from '@xyd-js/components/writer';
-import { useStyle } from '@xyd-js/components/utils';
+import { Badge } from '@xyd-js/components/writer';
 
 const meta: Meta<typeof Badge> = {
     title: 'Writer/Badge',
@@ -18,36 +17,4 @@ export const Default: Story = {
         children: 'Default Badge',
         kind: 'warning',
     },
-};
-
-// Custom styled version
-const CustomStyledBadge = () => {
-    const styled = useStyle<BadgeStyles>(Badge);
-    
-    // Override warning style
-    const warningClass = styled.BadgeHostWarning`
-        background-color: #fff3dc;
-        border: 2px solid #ffb020;
-    `;
-
-    // Override info style
-    const infoClass = styled.BadgeHostInfo`
-        background-color: #d8f3ff;
-        border: 2px solid #0288d1;
-    `;
-
-    return (
-        <div style={{ display: 'flex', gap: '16px' }}>
-            <Badge kind="warning" className={warningClass}>
-                Custom Warning Badge
-            </Badge>
-            <Badge kind="info" className={infoClass}>
-                Custom Info Badge
-            </Badge>
-        </div>
-    );
-};
-
-export const CustomStyled: Story = {
-    render: () => <CustomStyledBadge />,
 };
