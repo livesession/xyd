af2a825f3abc601cee989b041a61fdf0fcb315d5
Patryk Zdunowski <zdunekhere@gmail.com>
2025-11-01 15:28:11 +0100

    feat(website): fix build + website2 -> website

--- apps/website/src/app/PrimerProvider.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import { ThemeProvider } from "@primer/react-brand";
-
-export default function PrimerProvider({ children }: { children: React.ReactNode }) {
-    return <ThemeProvider colorMode="dark">
-        {children}
-    </ThemeProvider>
-}
--- apps/website/src/app/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export {
-    useActiveSection
-} from "./useActiveSection"
