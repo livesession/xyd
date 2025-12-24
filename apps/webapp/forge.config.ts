84670246011e451503977259df8b1aac442c5b9d
Patryk Zdunowski <zdunekhere@gmail.com>
2025-12-24 03:14:17 +0100

    feat(webapp2): try to fix github action

--- apps/webapp/forge.config.ts
+++ apps/webapp/forge.config.ts
@@ -10,6 +10,7 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 const config: ForgeConfig = {
   packagerConfig: {
     name: "xyd",
+    executableName: "xyd",
     asar: {
       unpack: '**/node_modules/xyd-js/**/*',
     },
